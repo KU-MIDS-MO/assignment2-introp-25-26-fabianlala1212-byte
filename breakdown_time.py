7f632e14317f0537e4220342409f87c2b7becfbf
unknown <fabianlala1212@gmail.com>
2025-11-10 22:12:29 +0100

    'done'

--- breakdown_time.py
+++ breakdown_time.py
@@ -1,4 +1,15 @@
 def breakdown_time(seconds):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if type(seconds) != int or seconds <0:
+        return -1
+    if seconds == 0:
+        return {}
+    units=[3600,60,1]
+    result = {}
+    remaining = seconds
+    for unit in units:
+        count = remaining // unit
+        if count >0:
+            result[unit] = count
+        remaining = remaining % unit
+    return result
+print(breakdown_time(7200))
--- swap_ends.py
+++ swap_ends.py
@@ -1,4 +1,7 @@
 def swap_ends(L, k):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if not L or k<=0 or k>len(L) // 2:
+        return (L.copy(), 0)
+    new_list = L[-k:] + L[k:len(L)-k] + L[:k]
+    num_swap= k
+    return(new_list , num_swap)
+print(swap_ends([10,20,40,50,60,70,80], 3))
